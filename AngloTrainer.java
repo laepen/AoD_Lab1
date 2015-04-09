51c9adbec474d538e3caac961b621db51f7aded2
laepen <tobnie@student.chalmers.se>
2015-04-09 15:42:06 +0200

    Done
    Funkar nu för ctrl+z, inte perfekt dock.

--- AngloTrainer.java
+++ AngloTrainer.java
@@ -1,9 +1,9 @@
 import java.io.BufferedReader;
+import java.io.Console;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
@@ -15,9 +15,13 @@ import java.util.TreeSet;
 // Date:	25-03-2015
 
 
+/**
+ * @author Habitat
+ *
+ */
 public class AngloTrainer
 {
-	
+
 	private TreeSet<String> dictionary; 
 	private Scanner scanner; 
 	public int longestWord;
@@ -27,7 +31,7 @@ public class AngloTrainer
 		randomGenerator = new Random();
 
 		dictionary = new TreeSet<String>();
-		
+
 		loadDictionary(dictionaryFile);
 	}
 
@@ -64,7 +68,7 @@ public class AngloTrainer
 	}
 
 	private String randomLetters( int length ) {
-		// this makes vovels a little more likely
+		// this makes vowels a little more likely
 		String letters = "aabcdeefghiijklmnoopqrstuuvwxyyz";  
 		StringBuffer buf = new StringBuffer(length);
 		for ( int i = 0; i < length; i++ ) 
@@ -78,7 +82,7 @@ public class AngloTrainer
 	 * Let #(x,s) = the number of occurrences of the charcter x in the string s.
 	 * includes(a,b) holds iff for every character x in b, #(x,b) <= #(x,a)
 	 * 
-	 * A neccessary precondition for includes is that both strings are sorted
+	 * A necessary precondition for includes is that both strings are sorted
 	 * in ascending order.
 	 */
 	private boolean includes( String a, String b ) {
@@ -101,80 +105,86 @@ public class AngloTrainer
 		return true;
 	}
 
-	public static void main(String[] args) throws IOException
-	{
-		AngloTrainer trainer = null;
+//	  takes a AngloTrainer object and the randomLetters string
+//	  puts the user input & randomLetters into separate treeSet, which are used to check wether a 
+//	  guessed word exists in the dictionary & only uses letters from the randomletters string.
+	public static void runGame(AngloTrainer trainer, String randomLetters) throws IOException{
 		String input;
-		char[] test1, test2;
+		char[] inputArray, randomLettersArray;
 		TreeSet<String> inputLetters = new TreeSet<String>(); 
 		TreeSet<String> sortedRandomLetters = new TreeSet<String>(); 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-		trainer = new AngloTrainer("dictionary.txt");
 		
-		//trainer.longestWord is length of longest word
-		String randomLetters = trainer.randomLetters(5);
-
-		System.out.println("The random letters are: " + randomLetters);
-		System.out.println("Try to form as many word as possible\n");
-	
+		
 		while(true){
 			input = br.readLine();
-			test1 = input.toCharArray();
-			for(Character x : test1){
+			if(input == null)
+				break;
+			if(br.toString().equals("\0"))
+				break;
+			
+			inputArray = input.toCharArray();
+			for(Character x : inputArray){
 				inputLetters.add(x.toString());
 			}
-			test2 = randomLetters.toCharArray();
-			for(Character x : test2){
+			randomLettersArray = randomLetters.toCharArray();
+			for(Character x : randomLettersArray){
 				sortedRandomLetters.add(x.toString());
 			}
 			boolean exists = false; 
-			exists = (trainer.dictionary.contains(input) && (inputLetters.contains(sortedRandomLetters.)))? true:false;
-			if(exists)
+			boolean matches = false; 
+//			check if the word exists in our dictionary			
+			exists = trainer.dictionary.contains(input) ? true:false;
+//			setting input = null lets us use ctrl+z to leave the program when we so wish
+			input = null; 
+//			check if the letters use matches our allowed letters
+			matches = sortedRandomLetters.containsAll(inputLetters) ? true:false;
+			if(exists && matches){
 				System.out.print("ok!");
+				inputLetters.clear();
+			}
+			else if(exists && !matches){
+				System.out.print("Your suggestion was found in the dictionary but,\n");
+				System.out.print("you used letters not in your given set.\n");
+				System.out.print("I found with the allowed letters: \n");
+				br.close();
+				break;
+			}
 			else{
 				System.out.println("Your suggestion was not found in the dictionary.");
 				System.out.println("I found: \n");
+				br.close();
 				break;
 			}			
 		}
-
-//		while(true)
-//		{
-//			input = br.readLine();
-//
-//			boolean exists = false;
-//
-//			for(String word : trainer.dictionary)
-//			{
-//				exists = input.equals(word) ? true : false;
-//				if(exists) break; 
-//			}
-//
-//			if(exists)
-//			{
-//				System.out.println("ok!");
-//			}
-//			else
-//			{
-//				System.out.println("Your suggestion was not found in the dictionary.");
-//				System.out.println("I found: \n");
-//				break;
-//			}
-//		}
-		
+	}
+	
+	public static void endState(AngloTrainer trainer, String randomLetters){
 		char [] newWord = randomLetters.toCharArray();
 		Arrays.sort(newWord);
-		
 		for(String w : trainer.dictionary)
 		{
 			char [] newDic = w.toCharArray();
 			Arrays.sort(newDic);
-			
 			if(trainer.includes(new String(newWord), new String(newDic)))
 			{
 				System.out.println(w);
 			}
 		}
+	}
+
+	public static void main(String[] args) throws IOException
+	{
+		AngloTrainer trainer = null;
+		trainer = new AngloTrainer("dictionary.txt");
+		//trainer.longestWord is length of longest word
+		//the longest word will always have a set number letter so we're using 5 for simplicity whilst testing.	
+		String randomLetters = trainer.randomLetters(5);
+
+		System.out.println("The random letters are: " + randomLetters);
+		System.out.println("Try to form as many word as possible\n");
+
+		runGame(trainer, randomLetters);
+		endState(trainer, randomLetters);	
 	}
 }
